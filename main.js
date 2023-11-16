7a710c87c537b44b97b5f60e3428cc4d4db4e821
gigio123 <128100488+gigio123@users.noreply.github.com>
2023-11-16 20:26:24 -0300

    Update main.js
    1611

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+ function tocaSomPom (idElementoAudio){
+    documento.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador <listaDeTeclas.length;contador++){
+
+const tecla = listaDeTeclas[contador];
+const instrumento = tecla.classList[1];
+const idAudio = `#som_${instrumento}`;//template string
+
+tecla.onclick = function () {
+    tocaSom(idAudio);
 }
 
-document.querySelectorAll('.tecla')
+tecla.onkeydown = function (evento){
+
+console.log(evento.code =='Space')
+
+if(evento.code === 'Space'){
+    tecla.classList.add('ativa');
+}
+
+if(evento.code ==='Enter'){
+    tecla.classList.add('ativa');
+}
+
+
+
+
+tecla.onkeyup = function(){
+    tecla.classList.remove('ativa');
+}
